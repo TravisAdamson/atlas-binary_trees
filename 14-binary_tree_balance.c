cabc9b745155cac086ef8387cea3516040a6e487
Travis <travis.adamson@atlasschool.com>
2024-10-23 16:13:38 -0500

    14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,6 @@
 #include "binary_trees.h"
-#include "9-binary_tree_height.c"
+
+int given_tree_height(const binary_tree_t *tree);
 
 /**
  * binary_tree_balance - Find balance factor of given binary tree
@@ -11,4 +12,29 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+}
+
+/**
+ * given_tree_height - Locates the height of a given tree
+ * @tree: Tree to examine
+ * Return: Height of given tree
+ */
+int given_tree_height(const binary_tree_t *tree)
+{
+	int left = 0, right = 0;
+
+	if (!tree)
+		return (0);
+	if (tree->left || tree)
+		left = given_tree_height(tree->left) + 1;
+	else
+		left = 0;
+	if (tree->right || tree)
+		right = given_tree_height(tree->right) + 1;
+	else
+		right = 0;
+	if (left > right)
+		return (left);
+	else
+		return (right);
 }
