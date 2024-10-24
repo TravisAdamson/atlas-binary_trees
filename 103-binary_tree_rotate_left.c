b599787c59df4dbbe4df0072ab0046a7affb99ba
Travis <travis.adamson@atlasschool.com>
2024-10-24 12:11:43 -0500

    103-binary_tree_rotate_left.c

---
+++ 103-binary_tree_rotate_left.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_rotate_left - Rotates the nodes to the left as needed
+ * @tree: Tree to rotate
+ * Return: New Root Node
+ */
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
+{
+	binary_tree_t *temp = NULL;
+
+	if (!tree)
+		return (NULL);
+	temp = tree->right;
+	temp->parent = tree->parent;
+	tree->right = temp->right;
+	if (temp->left)
+		temp->left->parent = tree;
+	temp->left = tree;
+	tree->parent = temp;
+	return (temp);
+}
